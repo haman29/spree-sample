d0378e4c56bb0401f6a5970e978bb977a5fa8103
Kyohei Hamada <kyohei.hamada@gmail.com>
2013-05-07 14:50:22 +0900

    execute `$ rails _3.2.13_ new spree-sample`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpreeSample::Application.config.secret_token = 'e03550146b74f2baca08c25ec87d8f10c4c68c75029566fb9d6a7463f37097f1c8e148ed2d6e612812cfe40b1ec1bbd1b4ba9dd10f5f77b56eed54f9b58b8025'
